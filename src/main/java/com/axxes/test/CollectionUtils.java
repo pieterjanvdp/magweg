a77692e6771683ddfa9554eb4ac296cc868ba4f0
unknown <pvandeperre@L-CND5095CTL.axxes.lan>
2015-09-01 16:50:07 +0200

    First commit

---
+++ src/main/java/com/axxes/test/CollectionUtils.java
@@ -0,0 +1,7 @@
+package com.axxes.test;
+
+/**
+ * Created by pvandeperre on 1/09/2015.
+ */
+public class CollectionUtils {
+}
---
+++ src/test/java/com/axxes/testp/CollectionUtilsTest.java
@@ -0,0 +1,17 @@
+package com.axxes.testp;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+/**
+ * Created by pvandeperre on 1/09/2015.
+ */
+public class CollectionUtilsTest {
+
+    @Test
+    public void testSum() throws Exception
+    {
+        assertEquals(4, 4);
+    }
+}
